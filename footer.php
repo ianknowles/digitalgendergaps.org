74fd62d8a6d73ec3b6dbf769d50e090a9955054d
ianknowles <ian@imknowles.co.uk>
2019-05-28 20:10:20 +0100

    use css styles for footer logos

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 	<hr>
 	<div class="d-flex flex-column flex-lg-row flex-wrap justify-content-center align-content-center">
 		<div class="align-self-center">A collaboration between</div>
-		<div class="align-self-center"><a href="https://www.sociology.ox.ac.uk/" target="_blank"><img class="m-2 p-2" style="background-color: #002147; height: 86px" alt="University of Oxford logo" src="<?php echo get_theme_file_uri('/assets/images/oxweb-logo-rect.svg'); ?>"></img></a></div>
-		<div class="align-self-center"><a href="https://www.qcri.org.qa/" target="_blank"><img class="m-2 p-2" style="height: 86px" alt="Hamad Bin Khalifa University logo" src="<?php echo get_theme_file_uri('/assets/images/qcri28rgb29-01.svg'); ?>"></img></a></div>
+		<div class="align-self-center"><a href="https://www.sociology.ox.ac.uk/" target="_blank"><img class="m-2 p-2 footer-logo footer-logo-oxford" alt="University of Oxford logo" src="<?php echo get_theme_file_uri('/assets/images/oxweb-logo-rect.svg'); ?>"></img></a></div>
+		<div class="align-self-center"><a href="https://www.qcri.org.qa/" target="_blank"><img class="m-2 p-2 footer-logo" alt="Hamad Bin Khalifa University logo" src="<?php echo get_theme_file_uri('/assets/images/qcri28rgb29-01.svg'); ?>"></img></a></div>
 		<div class="align-self-center">With support from</div>
-		<div class="align-self-center"><a href="https://www.data2x.org/" target="_blank"><img class="m-2 p-2" alt="data2x project logo" src="<?php echo get_theme_file_uri('/assets/images/data2x-logo.png'); ?>"></img></a></div>
+		<div class="align-self-center"><a href="https://www.data2x.org/" target="_blank"><img class="m-2 p-2 footer-logo" alt="data2x project logo" src="<?php echo get_theme_file_uri('/assets/images/data2x-logo.png'); ?>"></img></a></div>
 	</div>
 	<p class="m-2">&copy; 2018–<?=date("Y")?>, Digital Gender Gaps Project</p>
 </footer>
