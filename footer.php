6dd5e4681c8e33e65746bdcd65939f090ce6488a
ianknowles <ian@imknowles.co.uk>
2019-05-30 15:42:44 +0100

    add noopener for links to other domains for xs safety

--- footer.php
+++ footer.php
@@ -3,20 +3,20 @@
 	<div class="d-flex flex-column flex-lg-row flex-wrap justify-content-center align-content-center">
 		<div class="align-self-center">A collaboration between</div>
 		<div class="align-self-center">
-			<a href="https://www.sociology.ox.ac.uk/" target="_blank">
+			<a href="https://www.sociology.ox.ac.uk/" target="_blank" rel="noopener">
 				<img class="m-2 p-2 footer-logo footer-logo-oxford" alt="University of Oxford logo"
 					src="<?php echo get_theme_file_uri('/assets/images/oxweb-logo-rect.svg'); ?>"></img>
 			</a>
 		</div>
 		<div class="align-self-center">
-			<a href="https://www.qcri.org.qa/" target="_blank">
+			<a href="https://www.qcri.org.qa/" target="_blank" rel="noopener">
 				<img class="m-2 p-2 footer-logo" alt="Hamad Bin Khalifa University logo"
 					src="<?php echo get_theme_file_uri('/assets/images/qcri28rgb29-01.svg'); ?>"></img>
 			</a>
 		</div>
 		<div class="align-self-center">With support from</div>
 		<div class="align-self-center">
-			<a href="https://www.data2x.org/" target="_blank">
+			<a href="https://www.data2x.org/" target="_blank" rel="noopener">
 				<img class="m-2 p-2 footer-logo" alt="data2x project logo"
 					src="<?php echo get_theme_file_uri('/assets/images/data2x-logo.png'); ?>"></img>
 			</a>
