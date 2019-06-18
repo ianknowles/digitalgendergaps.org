a050a2e37c3de210762296ea63f4aa1af3eaf2eb
ianknowles <ian@imknowles.co.uk>
2019-06-18 23:29:51 +0100

    adjust palette picker borders to overlay the picker border on the label

--- template-parts/graph/graph-palettepicker.php
+++ template-parts/graph/graph-palettepicker.php
@@ -3,8 +3,8 @@
 								<div>
 									<div class="input-group d-flex">
 										<input type="color" id="<?php echo get_query_var('map-template-id'); ?>-color-min-input" class="align-self-stretch color-pick-size"></input>
-										<button type="button" id="<?php echo get_query_var('map-template-id'); ?>-color-min-button" class="btn btn-outline-secondary color-pick-size"></button>
-										<div class="input-group-append">
+										<button type="button" id="<?php echo get_query_var('map-template-id'); ?>-color-min-button" class="btn btn-outline-secondary color-pick-size border-right-2"></button>
+										<div class="input-group-append border-left-0">
 											<span class="input-group-text">
 												<a id="<?php echo get_query_var('map-template-id'); ?>-color-min-link">Min</a>
 											</span>
