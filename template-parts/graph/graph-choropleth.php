48e4431be074735a81eef28bf8fc6e24da058a35
ianknowles <ian@imknowles.co.uk>
2019-06-18 02:11:31 +0100

    split map legends into template parts

--- template-parts/graph/graph-choropleth.php
+++ template-parts/graph/graph-choropleth.php
@@ -5,45 +5,11 @@
 							<div class="col-12 col-lg-11 d-lg-flex flex-lg-column pr-lg-0">
 								<div id="<?php echo get_query_var('map-template-id'); ?>-chart-area" class="border background-color-sea"></div>
 							</div>
-							<div id="<?php echo get_query_var('map-template-id'); ?>-v-legend" class="col-lg-1 d-none d-lg-flex flex-lg-column pl-0">
-								<div class="d-flex flex-row h-100">
-									<div id="<?php echo get_query_var('map-template-id'); ?>-v-legend-gradient" class="align-self-center mr-1" style="width: 15px; height: 95%;"></div>
-									<div id="<?php echo get_query_var('map-template-id'); ?>-v-legend-values" class="d-flex flex-column justify-content-between">
-										<div class="font-weight-bold"></div>
-										<div></div>
-										<div></div>
-										<div></div>
-										<div></div>
-										<div></div>
-										<div></div>
-										<div></div>
-										<div></div>
-										<div></div>
-										<div class="font-weight-bold"></div>
-									</div>
-								</div>
-							</div>
+							<?php get_template_part('template-parts/graph/graph', 'legend-v'); ?>
 						</div>
-						<div id="<?php echo get_query_var('map-template-id'); ?>-h-legend" class="row d-flex d-lg-none">
-							<div class="col-12 col-lg-11">
-								<div class="row d-flex justify-content-center align-items-center">
-									<div id="<?php echo get_query_var('map-template-id'); ?>-h-legend-gradient" style="width: 92.5%; height: 15px;"></div>
-								</div>
-								<div id="<?php echo get_query_var('map-template-id'); ?>-h-legend-values" class="d-flex flex-row justify-content-between">
-									<div class="font-weight-bold"></div>
-									<div class="d-none d-sm-block"></div>
-									<div></div>
-									<div class="d-none d-sm-block"></div>
-									<div></div>
-									<div class="d-none d-sm-block"></div>
-									<div></div>
-									<div class="d-none d-sm-block"></div>
-									<div></div>
-									<div class="d-none d-sm-block"></div>
-									<div class="font-weight-bold"></div>
-								</div>
-							</div>
-						</div>
-						<?php get_template_part('template-parts/graph/graph', 'palettepicker'); ?>
+						<?php
+							get_template_part('template-parts/graph/graph', 'legend-h');
+							get_template_part('template-parts/graph/graph', 'palettepicker');
+						?>
 						<figcaption class="sr-only">Prediction map</figcaption>
 					</figure>
