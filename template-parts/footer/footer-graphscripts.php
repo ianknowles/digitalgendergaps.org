ef4fa0c54e7660674498e659dde03df1d46bd0ea
ianknowles <ian@imknowles.co.uk>
2020-01-21 12:52:42 +0000

    update vendor graph scripts

--- template-parts/footer/footer-graphscripts.php
+++ template-parts/footer/footer-graphscripts.php
@@ -1,23 +1,10 @@
 <!-- Graphs -->
-<script
-	src="https://d3js.org/d3.v3.min.js"
-	integrity="sha384-N8EP0Yml0jN7e0DcXlZ6rt+iqKU9Ck6f1ZQ+j2puxatnBq4k9E8Q6vqBcY34LNbn"
-	crossorigin="anonymous">
-</script>
-<script
-	src="https://d3js.org/topojson.v1.min.js"
-	integrity="sha384-3lqRfvBKsOqlSuKt8TGRZx9QHpUtHNtqlznKwCVbDqgX30Evy7TC2iGRAElF9QGK"
-	crossorigin="anonymous">
-</script>
-<script
-	src="https://d3js.org/d3.geo.projection.v0.min.js"
-	integrity="sha384-GHb5+5OkyovCRQooOGCbrFWx7BkUQ6SJqdQyBrnIhbpQiU0HHBiBWWtecGllqbRQ"
-	crossorigin="anonymous">
-</script>
-<script
-	src="https://d3js.org/d3-queue.v3.min.js"
-	integrity="sha384-puzhGp6u77up1zwVzjpJTZdrvYHJ7CpqgKb7Ibh9rlhDtmHYKy3i8IUhZv2fBkUc"
-	crossorigin="anonymous">
+<script src="<?php echo get_theme_file_uri('/assets/js/vendor/d3.v5.min.js'); ?>"></script>
+<script src="https://d3js.org/d3-array.v1.min.js"></script>
+<script src="https://d3js.org/d3-geo.v1.min.js"></script>
+<script src="https://d3js.org/d3-geo-projection.v2.min.js"></script>
+<script src="<?php echo get_theme_file_uri('/assets/js/vendor/c3.min.js'); ?>"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/topojson/1.6.9/topojson.min.js"></script>
 </script>
 
 <script src="<?php echo get_theme_file_uri('/assets/js/barchart.js'); ?>"></script>
