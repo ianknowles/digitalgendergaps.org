a5c670a775ea6e02bcebc49ed502f139a5d0fff9
ianknowles <ian@imknowles.co.uk>
2020-01-21 12:52:01 +0000

    switch to barchart javascript

--- template-parts/footer/footer-graphscripts.php
+++ template-parts/footer/footer-graphscripts.php
@@ -19,16 +19,6 @@
 	integrity="sha384-puzhGp6u77up1zwVzjpJTZdrvYHJ7CpqgKb7Ibh9rlhDtmHYKy3i8IUhZv2fBkUc"
 	crossorigin="anonymous">
 </script>
-<script
-	src="https://datamaps.github.io/scripts/0.4.4/datamaps.world.min.js"
-	integrity="sha384-hVQQr5ElKsQUvFyZD4XpXGPbWSPyNVRfQix43Fh4Z4QQ4/cTbBobC9YW9Rr4GHbs"
-	crossorigin="anonymous">
-</script>
-<script src="<?php echo get_theme_file_uri('/assets/js/dashboard.js'); ?>"></script>
-<?php
-//<script
-//	src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"
-//	integrity="sha384-U3SHMuMFK4M7q42jpyqaAHJTzci8BikdB2ZgmUEsrElciy0Ty1vxiW4EL3bNCsyT"
-//	crossorigin="anonymous">
-//</script>
-?>
+
+<script src="<?php echo get_theme_file_uri('/assets/js/barchart.js'); ?>"></script>
+
